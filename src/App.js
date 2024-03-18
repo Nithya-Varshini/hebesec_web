105a3444e083e9ef9bdd6a03209b8587004d921c
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-03-18 11:20:42 +0530

    about - neha push

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Container, Row, Col } from 'react-bootstrap';
 
 import './App.css';
+import { About } from './components/about/about';
 
 function App() {
   return (
     <BrowserRouter>
       <Navv />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          {/* <Route path='/store/:LinkCategory?' element={<Store storeData={storeData} setStoreData={setStoreData} setCartActive={setCartActive} fetchStoreData={fetchStoreData} cartCount={cartCount} fetchCartCount={fetchCartCount} />} />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+         {/* <Route path='/store/:LinkCategory?' element={<Store storeData={storeData} setStoreData={setStoreData} setCartActive={setCartActive} fetchStoreData={fetchStoreData} cartCount={cartCount} fetchCartCount={fetchCartCount} />} />
           <Route path='/product/:category/:id' element={<ProductPage products={storeData?.products} setData={setStoreData} setCartActive={setCartActive} fetchStoreData={fetchStoreData} cartCount={cartCount} fetchCartCount={fetchCartCount} />} />
           <Route path='/checkout' element={<Checkout products={storeData?.products} fetchStoreData={fetchStoreData} />} />
-          <Route path='/booking' element={<Booking />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/programs' element={<Programs />} /> */}
-        </Routes>
+          <Route path='/booking' element={<Booking />} /> */}
+        <Route path='/about' element={<About />} />
+        {/* <Route path='/programs' element={<Programs />} />  */}
+      </Routes>
     </BrowserRouter>
   );
 }
